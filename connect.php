26e700bda8cd44519310ff516f8c3f6812b26f02
jesenjintech <122051785+jesenjintech@users.noreply.github.com>
2023-01-30 22:47:15 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,35 @@
 <?php
-$ime = $_POST['ime'];
-$prezime = $_POST['prezime'];
 
-$conn = new mysqli('ocalhost','root','','test');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
-}else{
-    $stmt = $conn->prepare("insert into registration(ime,prezime)
-    values(?,?)");
-    $stmt->bind_parm("ss",$ime, $prezime);
-    $stmt->execute();
-    echo "registration successfully";
-    $stmt->close();
-    $conn->close();
+// Konekcija na bazu podataka
+$servername = "localhost";
+$username = "your_db_username";
+$password = "your_db_password";
+$dbname = "your_db_name";
+
+// Kreiranje konekcije
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Provjera konekcije
+if (!$conn) {
+    die("Konekcija na bazu podataka nije uspjela: " . mysqli_connect_error());
+}
+
+// Preuzimanje podataka iz forme
+$ime = $_POST["ime"];
+$prezime = $_POST["prezime"];
+
+// Kreiranje SQL upita za čuvanje podataka
+$sql = "INSERT INTO customers (ime, prezime)
+VALUES ('$ime', '$prezime')";
+
+// Izvršenje upita
+if (mysqli_query($conn, $sql)) {
+    echo "Uspješno ste zakazali termin";
+} else {
+    echo "Došlo je do greške: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Zatvaranje konekcije
+mysqli_close($conn);
+
 ?>
