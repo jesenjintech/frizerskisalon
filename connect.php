87f1bcb8854cab5c2abed43e2c5083a33018764e
jesenjintech <122051785+jesenjintech@users.noreply.github.com>
2023-01-31 13:11:01 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 // Konekcija na bazu podataka
 $servername = "localhost";
-$username = "your_db_username";
-$password = "your_db_password";
-$dbname = "your_db_name";
+$username = "bojan123";
+$password = "bojan123";
+$dbname = "bojan123";
 
 // Kreiranje konekcije
 $conn = mysqli_connect($servername, $username, $password, $dbname);
